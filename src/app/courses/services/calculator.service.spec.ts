6b3b2d7f6c2ad5058460194d3cdb68e326753dfd
Paul DAO <dao.van.paul@gmail.com>
2022-01-19 16:28:24 -0500

    TEST calculator service mock logger service, use beforeEach to set variable

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -2,15 +2,23 @@ import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 
 describe('CalculatorService', () => {
+  let calculator: CalculatorService, loggerSpy: any;
+
+  // Values will be initialized for each test. To avoid conflict between unit tests
+  beforeEach(() => {
+    // Fake object represents LoggerService
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+    calculator = new CalculatorService(loggerSpy);
+  });
   it('should be two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 
   it('should be two numbers', () => {
-    const calculator = new CalculatorService(new LoggerService());
     const result = calculator.subtract(2, 2);
     expect(result).toBe(0);
+    expect(loggerSpy.log).toHaveBeenCalledTimes(1);
   });
 });
