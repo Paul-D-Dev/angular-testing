885e39ecc25dfb509e1603bf7038ac848b2fc1c1
Paul DAO <dao.van.paul@gmail.com>
2022-01-20 10:43:11 -0500

    TEST calculator unit test calculator service

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,3 +1,4 @@
+import { TestBed } from '@angular/core/testing';
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
 
@@ -6,10 +7,20 @@ describe('CalculatorService', () => {
 
   // Values will be initialized for each test. To avoid conflict between unit tests
   beforeEach(() => {
-    // Fake object represents LoggerService
+    // Fake object represents LoggerService, create completely a mock service
+    // We're doing this because we want only test CalculatorService and not his dependencies.
     loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
-    calculator = new CalculatorService(loggerSpy);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        { provide: LoggerService, useValue: loggerSpy },
+      ],
+    });
+
+    calculator = TestBed.inject(CalculatorService);
   });
+
   it('should be two numbers', () => {
     const result = calculator.add(2, 2);
     expect(result).toBe(4);
