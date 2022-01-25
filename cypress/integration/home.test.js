80185b92036a2d6b8980f319600ecf08f5133c0a
Paul DAO <dao.van.paul@gmail.com>
2022-01-25 11:46:49 -0500

    TEST E2E : home page load mock response data, simulate call api, check display list courses

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,7 +1,21 @@
 describe('Home Page', () => {
-  it("should display a list of courses", () => {
+  it('should display a list of courses', () => {
+
+    // Load mock response api/courses
+    cy.fixture('courses.json').as('coursesJson');
+
+    // Start fake server
+    cy.server();
+    // Route to api/courses, using data from file coursesJson
+    cy.route('/api/courses', '@coursesJson').as('courses');
     cy.visit('/');
     cy.contains('All Courses');
+
+    // Wait the response completed
+    cy.wait('@courses');
+    // Check if we got courses beginners (9)
+    cy.get('mat-card').should('have.length', 9);
+
   });
 
 
