57634082683dde3620148ddf4d4707948bfc5dd4
Paul DAO <dao.van.paul@gmail.com>
2022-01-25 12:51:02 -0500

    TEST E2E : home page simulate click user, display advanced course, check title value

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,6 +1,7 @@
 describe('Home Page', () => {
-  it('should display a list of courses', () => {
 
+  // Refactor common part
+  beforeEach(() => {
     // Load mock response api/courses
     cy.fixture('courses.json').as('coursesJson');
 
@@ -9,13 +10,32 @@ describe('Home Page', () => {
     // Route to api/courses, using data from file coursesJson
     cy.route('/api/courses', '@coursesJson').as('courses');
     cy.visit('/');
+  })
+  it('should display a list of courses', () => {
     cy.contains('All Courses');
 
     // Wait the response completed
     cy.wait('@courses');
-    // Check if we got courses beginners (9)
+    // Check if we got beginners courses (9)
     cy.get('mat-card').should('have.length', 9);
+  });
 
+  it('should display the advanced courses', () => {
+    const tabs = cy.get('.mat-tab-label');
+    // Check tabs
+    tabs.should('have.length', 2);
+
+    // Simulate click on Advanced tab
+    tabs.last().click();
+
+    // Count how much title and check first title value
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .its('length')
+      .should('be.gt',1);
+
+    cy.get('.mat-tab-body-active .mat-card-title')
+      .first()
+      .should('contain', 'Angular Security Course')
   });
 
 
